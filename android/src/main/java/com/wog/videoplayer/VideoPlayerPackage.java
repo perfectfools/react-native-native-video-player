bf857810148004b118683fff6ad883f43f1312c9
Mikael Palm <mikpal@perfectfools.com>
2020-01-16 14:34:18 +0100

    minor fixes

--- android/src/main/java/com/wog/videoplayer/VideoPlayerPackage.java
+++ android/src/main/java/com/wog/videoplayer/VideoPlayerPackage.java
@@ -20,6 +20,9 @@ public class VideoPlayerPackage implements ReactPackage {
         return Arrays.<NativeModule>asList(new VideoPlayerModule(reactContext));
     }
 
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
